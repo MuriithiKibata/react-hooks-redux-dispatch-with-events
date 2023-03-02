7e3c05275b05b80d3d118c17da58bc70219dcb54
Muriithi <muriithi.kibata@student.moringaschool.com>
2023-03-02 16:33:15 +0300

    Done

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,26 @@
 // add code snippets from README
+let state;
+
+function reducer(state= {count: 0}, action) {
+    switch(action.type) {
+        case "counter/increment":
+          return {count: state.count + 1}
+            break
+            default:
+                return state
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+ 
+}
+
+function render() {
+    let cont = document.getElementById("container");
+    cont.textContent = state.count;
+    dispatch({ type: "counter/increment"});
+}
+
+document.getElementById("button").addEventListener("click", render);
+
